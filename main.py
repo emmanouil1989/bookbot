3b67d7b3d2f1f0012c91ac34bb439571f5101c60
emmanouil emmanouil <emmanouil.e25@gmail.com>
2025-01-13 21:47:01 +0000

    count characters

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def main():
     file_path = 'books/frankenstein.txt'
     file_contents = read_contents_from_path(file_path)
-    words_count = count_words_in_file(file_contents)
+    num_words = count_words_in_file(file_contents)
+    characters_count = count_characters_in_file(file_contents)
     print(file_contents)
     print(f"{num_words} words found in the document")
+    print(characters_count)
+    
 
 def read_contents_from_path(file_path):
     with open(file_path) as f:
@@ -13,5 +16,16 @@ def read_contents_from_path(file_path):
 def count_words_in_file(file_contents):
     words = file_contents.split()
     return len(words)
+
+def count_characters_in_file(file_contents):
+    count_dictionary = {}
+    for char in file_contents:
+        lower_char = char.lower()
+        if lower_char in count_dictionary:
+            count_dictionary[lower_char] += 1
+        else:
+            count_dictionary[lower_char] = 1
+    return count_dictionary
+             
 
 main()
