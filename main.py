742752c09872c41d7daf7531613c384e99eedeab
emmanouil emmanouil <emmanouil.e25@gmail.com>
2025-01-13 21:33:22 +0000

    refactor in flexible code

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
-    with open('books/frankenstein.txt') as f:
+    file_path = 'books/frankenstein.txt'
+    file_contents = read_contents_from_path(file_path)
+    print(file_contents)
+
+def read_contents_from_path(file_path):
+    with open(file_path) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
 main()
